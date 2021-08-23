2aafbc03833d830c1dd0895e0e52998840f94e7d
Fernando Brito <fernando.brito@voiapp.io>
2021-08-23 18:45:47 +0200

    chore: apply lint

--- src/investmentstk/plots/correlation.py
+++ src/investmentstk/plots/correlation.py
@@ -3,18 +3,23 @@ import plotly.express as px
 import scipy.cluster.hierarchy as sch
 
 
-def plot_correlation_matrix(dataframe, **kwargs):
+def plot_correlation_matrix(dataframe, *, interactive=False, **kwargs):
     """
     Useful references:
     * https://towardsdatascience.com/better-heatmaps-and-correlation-matrix-plots-in-python-41445d0f2bec
     * https://stackoverflow.com/questions/52787431/create-clusters-using-correlation-matrix-in-python
     * https://github.com/TheLoneNut/CorrelationMatrixClustering/blob/master/CorrelationMatrixClustering.ipynb
 
+    :param interactive:
     :param dataframe:
     :return:
     """
     fig = px.imshow(dataframe, zmin=-1, zmax=1, color_continuous_scale="rdbu", **kwargs)
-    fig.show()
+
+    if interactive:
+        fig.show()
+    else:
+        fig.show("png")
 
 
 def discrete_colorscale(boundaries, colors):
@@ -30,11 +35,10 @@ def discrete_colorscale(boundaries, colors):
     returns the plotly  discrete colorscale
     """
     if len(boundaries) != len(colors) + 1:
-        raise ValueError('len(boundary values) should be equal to  len(colors)+1')
+        raise ValueError("len(boundary values) should be equal to  len(colors)+1")
 
     boundaries = sorted(boundaries)
-    nvals = [(v - boundaries[0]) / (boundaries[-1] - boundaries[0]) for v in
-             boundaries]  # normalized values
+    nvals = [(v - boundaries[0]) / (boundaries[-1] - boundaries[0]) for v in boundaries]  # normalized values
 
     colorscale = []  # discrete colorscale
 
@@ -56,10 +60,12 @@ def cluster_by_correlation(df):
 
 
 def format_tick_values(boundaries):
-    return [np.mean(boundaries[k:k + 2]) for k in
-            range(len(boundaries) - 1)]
+    return [np.mean(boundaries[k : k + 2]) for k in range(len(boundaries) - 1)]
 
 
 def format_tick_text(boundaries):
-    return [f'<{boundaries[1]}'] + [f'{boundaries[k]}-{boundaries[k + 1]}' for k in
-                                    range(1, len(boundaries) - 2)] + [f'>{boundaries[-2]}']
+    return (
+        [f"<{boundaries[1]}"]
+        + [f"{boundaries[k]}-{boundaries[k + 1]}" for k in range(1, len(boundaries) - 2)]
+        + [f">{boundaries[-2]}"]
+    )
