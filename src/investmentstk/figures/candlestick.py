11591f26343e1fdfe3ec57d4c7a54ad53b51a7d6
Fernando Brito <fernando.brito@voiapp.io>
2021-11-06 16:40:37 +0100

    feat: add support for candlestick graph with ATR subplot

--- src/investmentstk/figures/candlestick.py
+++ src/investmentstk/figures/candlestick.py
@@ -2,6 +2,7 @@ from datetime import timedelta, date
 
 import pandas as pd
 import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 
 from investmentstk.data_feeds.data_feed import TimeResolution
 from investmentstk.models.asset import Asset
@@ -17,12 +18,22 @@ def generate_figure(dataframe: pd.DataFrame, asset: Asset) -> go.Figure:
         name=asset.name,
     )
 
-    figure = go.Figure(data=[candlestick_chart])
+    # figure = go.Figure(data=[candlestick_chart])
+    figure = make_subplots(rows=2, cols=1, row_heights=[0.8, 0.2])
 
-    figure.update_layout(title="Candlestick chart")
+    figure.add_trace(candlestick_chart, row=1, col=1)
+
+    # Not easy to find good linear dtick with a log scale
+    data_range = dataframe["close"].max() - dataframe["close"].min()
+    figure.update_yaxes(row=1, col=1, type="log", dtick=f"L{data_range / 10}")
+
+    figure.update_layout(xaxis_rangeslider_visible=False)
+
+    figure.update_layout(title=f"{asset.name}: Candlestick chart")
 
     if "stop" in dataframe.columns:
-        figure.update_xaxes(rangebreaks=[dict(values=list(date_gaps(dataframe)))])
+        # TODO: only needed for daily charts. Find a way to figure it out from here
+        # figure.update_xaxes(rangebreaks=[dict(values=list(date_gaps(dataframe)))])
 
         # Plotly doesn't support one line chart with multiple colors. The recommended solution
         # is to have 2 different traces.
@@ -34,6 +45,8 @@ def generate_figure(dataframe: pd.DataFrame, asset: Asset) -> go.Figure:
             line_color="red",
             line_dash="dash",
             name="Stop loss (short)",
+            row=1,
+            col=1,
         )
 
         figure.add_scatter(
@@ -43,11 +56,24 @@ def generate_figure(dataframe: pd.DataFrame, asset: Asset) -> go.Figure:
             line_color="green",
             line_dash="dash",
             name="Stop loss (long)",
+            row=1,
+            col=1,
         )
 
-        figure.update_layout(title="Trailing stop loss (ATR indicator)")
+        figure.add_scatter(
+            x=dataframe.index, y=dataframe["atr"], mode="lines", line_color="black", name="ATR", row=2, col=1
+        )
+
+        figure.update_layout(title=f"{asset.name}: Trailing stop loss (ATR indicator)")
 
     figure.update_layout(legend=dict(yanchor="top", y=0.99, xanchor="left", x=0.01))
+
+    # Unifies the tooltips from multiple traces belonging to the same subplot into 1
+    figure.update_layout(hovermode="x unified")
+
+    # Extends the spike line for both subplots
+    # Found on https://community.plotly.com/t/unified-hovermode-with-sublots/37606
+    figure.update_traces(xaxis="x1")
 
     return figure
 
