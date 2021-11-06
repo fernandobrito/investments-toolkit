52736ce493c3063fcc0202f983bdda70dcc29513
Fernando Brito <fernando.brito@voiapp.io>
2021-11-06 16:38:57 +0100

    refactor: requests cache

--- src/investmentstk/persistence/requests_cache.py
+++ src/investmentstk/persistence/requests_cache.py
@@ -3,25 +3,31 @@ from datetime import timedelta
 from pathlib import Path
 
 import requests_cache
-from requests_cache import FileCache, json_serializer
+from requests_cache import json_serializer
 
 current_folder = Path(__file__).resolve().parent
 
-"""
-Default cache options for a filesystem backend.
-https://requests-cache.readthedocs.io/en/v0.7.4/modules/requests_cache.backends.html#module-requests_cache.backends.filesystem
-"""
-file_cache = FileCache(cache_name=current_folder / "../../.." / "cache" / "http_cache", serializer=json_serializer)
-
 
 def cache_default_options(hours: float = 24):
-    return dict(expire_after=timedelta(hours=hours))
+    """
+    Default cache options for a filesystem backend.
+    https://requests-cache.readthedocs.io/en/v0.7.4/modules/requests_cache.backends.html#module-requests_cache.backends.filesystem
+    """
+
+    return dict(
+        backend="filesystem",
+        expire_after=timedelta(hours=hours),
+        cache_name=current_folder / "../../.." / "cache" / "http_cache",
+        serializer=json_serializer,
+    )
 
 
 @contextmanager
-def requests_cache_configured(hours: float = 24):
+def requests_cache_configured(*, hours: float = 24, **kwargs):
     """
     Wrapper around a configured requests_cache context manager
     """
-    with requests_cache.enabled(backend=file_cache, **cache_default_options(hours=hours)):
+    args = {**cache_default_options(hours=hours), **kwargs}
+
+    with requests_cache.enabled(**args):
         yield
